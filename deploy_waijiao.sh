ee00ed8dc1cf85b2357cfe50c8db2cdafc5273d4
宋传胜 <songchuansheng@91waijiao.com>
2013-09-17 17:34:35 +0800

    extract base deploy functions

--- deploy_waijiao.sh
+++ deploy_waijiao.sh
@@ -2,38 +2,17 @@
 
 ################# checkout projects
 
-
-GIT_REPO=git@192.168.11.11:liuhua/php_91waijiao.git
 PACKAGE=waijiao
+GIT_REPO=git@192.168.11.11:liuhua/php_91waijiao.git
+INVENTORY=ansible_inventory
+SSHKEY=~/.vagrant.d/insecure_private_key
 LOCAL_PATH=./local/$PACKAGE
 
-if [ ! -d $LOCAL_PATH ]; then
-  git clone $GIT_REPO $LOCAL_PATH
-else
-  pushd .
-  cd $LOCAL_PATH
-  # remove any local unstaged changes
-  git clean -f
-  git reset --hard
-  # checkout master
-  git checkout master
-  # pull lastest change
-  git pull origin master
-  popd
-fi
+DIR=`dirname $0`
+PROG=`basename $0`
 
-################# make packages
+. $DIR/functions
 
-#cp ./bin/deploy_bootstrap.sh $LOCAL_PATH/.
-#./bin/makeself.sh $LOCAL_PATH deploy-`date "+%Y%m%d%H%M%S"`.run "deploy installer" ./deploy_bootstrap.sh
-
-pushd .
-cd $LOCAL_PATH/..
-tar --exclude=.git* --exclude=*.tar.gz -czf $PACKAGE.tar.gz .
-popd
-/bin/cp -f $LOCAL_PATH/../$PACKAGE.tar.gz roles/$PACKAGE/files
-
-################# deploy to remote system
-
-ansible-playbook -v --private-key=~/.vagrant.d/insecure_private_key  -i ansible_inventory $PACKAGE.yml
-
+checkout $GIT_REPO $LOCAL_PATH 
+build_package $PACKAGE $LOCAL_PATH 
+install_package $PACKAGE $SSHKEY $INVENTORY 
