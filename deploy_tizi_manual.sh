090875e4c66c035220fd97b43556a70fc9e12bda
宋传胜 <songchuansheng@91waijiao.com>
2013-09-30 15:40:15 +0800

    deploy tizi without ansible for tizi

---
+++ deploy_tizi_manual.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+################# checkout projects
+
+DIR=`dirname $0`
+PROG=`basename $0`
+
+usage() {
+  cat <<EOF
+usage:
+  $PROG -p package -b branch -g git_repo -k sshkey -i inventory_file -l limit_hosts -t ansible_tag
+
+OPTIONS:
+  -p   package name
+  -g   git repotory
+  -b   checkout specific branch
+  -i   ansible inventory file 
+  -k   use ssh private key
+  -l   limit hosts
+  -t   ansible tags
+  -h   help
+
+EOF
+}
+
+PACKAGE=tizi
+GIT_REPO=git@192.168.11.11:liuhua/zujuan_teacher.git
+SSHKEY=./ssh/ansible_id_rsa
+INVENTORY=production
+BRANCH=master
+TAG=tizi
+LIMIT=tizi-webserver
+
+while getopts "hp:g:i:k:t:l:b:" OPTION
+do
+  case $OPTION in
+    p)
+      PACKAGE=$OPTARG
+      ;;
+    g)
+      GIT_REPO=$OPTARG
+      ;;
+    i)
+      INVENTORY=$OPTARG
+      ;;
+    k)
+      SSHKEY=$OPTARG
+      ;;
+    l)
+      LIMIT=$OPTARG
+      ;;
+    b)
+      BRANCH=$OPTARG
+      ;;
+    t)
+      TAG=$OPTARG
+      ;;
+    h)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+LOCAL_PATH=./local/$PACKAGE
+
+. $DIR/functions
+
+checkout $GIT_REPO $LOCAL_PATH $BRANCH
+build_package $PACKAGE $LOCAL_PATH 
+install_package_manual 9191 waijiao 121.199.20.59 $LOCAL_PATH $PACKAGE
+
