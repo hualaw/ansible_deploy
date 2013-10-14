9a3ac3fdd7d375321c465c0d5e7a4c4eb3b45e69
宋传胜 <songchuansheng@91waijiao.com>
2013-10-14 17:05:54 +0800

    deploy phone service script

---
+++ deploy_phone.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+################# checkout projects
+
+DIR=`dirname $0`
+PROG=`basename $0`
+
+usage() {
+  cat <<EOF
+usage:
+  $PROG -p package -b branch -g git_repo -k sshkey -i inventory_file -t ansible_tag -u deploy_user
+
+OPTIONS:
+  -p   package name
+  -g   git repotory
+  -b   checkout specific branch
+  -i   ansible inventory file 
+  -k   use ssh private key
+  -t   ansible tags
+  -h   help
+
+EOF
+}
+
+PACKAGE=phone
+GIT_REPO=git@192.168.11.11:chris/phone_server.git
+SSHKEY=./ssh/ansible_id_rsa
+INVENTORY=production
+BRANCH=master
+TAG=phone
+LIMIT=phone
+
+#SERVERS=(112.124.41.101)
+DEPLOY_USER=deploy
+
+while getopts "hp:g:i:k:t:b:" OPTION
+do
+  case $OPTION in
+    p)
+      PACKAGE=$OPTARG
+      ;;
+    g)
+      GIT_REPO=$OPTARG
+      ;;
+    i)
+      INVENTORY=$OPTARG
+      ;;
+    k)
+      SSHKEY=$OPTARG
+      ;;
+    b)
+      BRANCH=$OPTARG
+      ;;
+    t)
+      TAG=$OPTARG
+      ;;
+    u)
+      DEPLOY_USER=$OPTARG
+      ;;
+    h)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+LOCAL_PATH=./local/$PACKAGE
+
+. $DIR/functions
+
+checkout $GIT_REPO $LOCAL_PATH $BRANCH
+build_package $PACKAGE $LOCAL_PATH
+install_package $PACKAGE $SSHKEY $INVENTORY $TAG $LIMIT
+
